3289f9f0d5b70876c7a3e2b9ce8380f93babe8f2
EliasDerHai <eliashaider95@gmail.com>
2023-11-28 21:08:48 +0100

    add file-name display and play button

--- src/audio_visualizer.rs
+++ src/audio_visualizer.rs
@@ -1,18 +1,19 @@
+use iced::widget::Button;
+use iced::widget::Column;
 use iced::widget::Text;
 use iced::{Application, Command};
-use iced::widget::Button;
 use native_dialog::FileDialog;
 use std::path::PathBuf;
 
-pub struct AudioVisualizer{
-    file_path: Option<PathBuf>
+pub struct AudioVisualizer {
+    file_path: Option<PathBuf>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     OpenPressed,
-    // Other messages...
- }
+    PlayPressed,
+}
 
 impl Application for AudioVisualizer {
     type Executor = iced::executor::Default;
@@ -20,9 +21,8 @@ impl Application for AudioVisualizer {
     type Flags = ();
     type Theme = iced::Theme;
 
-    
     fn new(_flags: Self::Flags) -> (AudioVisualizer, Command<Self::Message>) {
-        (AudioVisualizer{ file_path: None}, Command::none())
+        (AudioVisualizer { file_path: None }, Command::none())
     }
 
     fn title(&self) -> String {
@@ -34,29 +34,43 @@ impl Application for AudioVisualizer {
             Message::OpenPressed => {
                 match FileDialog::new()
                     .add_filter("Audio Files", &["mp3"])
-                    .show_open_single_file() 
+                    .show_open_single_file()
                 {
                     Ok(Some(path)) => {
                         println!("File selected: {:?}", path.file_name());
                         self.file_path = Some(path);
-                    },
+                    }
                     Ok(None) => {
                         self.file_path = None;
-                    },
+                    }
                     Err(err) => {
                         println!("File dialog error: {:?}", err);
                         self.file_path = None;
                     }
                 }
-            },
+            }
+            Message::PlayPressed => {
+                println!("Start pressed")
+            }
         }
-     
-        Command::none()    
+
+        Command::none()
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        Button::new(Text::new("Open")).on_press(Message::OpenPressed).into()
-        // Text::new(greeting).into()
+        let file_name = self
+            .file_path
+            .as_ref()
+            .and_then(|path| path.file_name())
+            .and_then(|os_str| os_str.to_str())
+            .map(|s| s.to_string())
+            .unwrap_or("-".to_string());
+
+        let open_button = Button::new(Text::new("Open")).on_press(Message::OpenPressed);
+        let file_text = Text::new(file_name);
+        let play_button = Button::new(Text::new("Play")).on_press(Message::PlayPressed);
+
+        Column::new().push(open_button).push(file_text).push(play_button).into()
     }
 
     fn theme(&self) -> Self::Theme {
