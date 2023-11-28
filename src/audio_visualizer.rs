0ec16469cd4456f334fd4eb9140f10c6a3906ebf
EliasDerHai <eliashaider95@gmail.com>
2023-11-28 22:11:23 +0100

    compiling but runtime error 'no device'

--- src/audio_visualizer.rs
+++ src/audio_visualizer.rs
@@ -1,12 +1,16 @@
-use iced::widget::Button;
-use iced::widget::Column;
-use iced::widget::Text;
-use iced::{Application, Command};
+use iced::{
+    widget::{Button, Column, Text},
+    Application, Command,
+};
 use native_dialog::FileDialog;
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink, Source};
+use std::fs::File;
 use std::path::PathBuf;
 
 pub struct AudioVisualizer {
     file_path: Option<PathBuf>,
+    audio_output: Option<OutputStreamHandle>,
+    audio_sink: Option<Sink>,
 }
 
 #[derive(Debug, Clone)]
@@ -22,7 +26,15 @@ impl Application for AudioVisualizer {
     type Theme = iced::Theme;
 
     fn new(_flags: Self::Flags) -> (AudioVisualizer, Command<Self::Message>) {
-        (AudioVisualizer { file_path: None }, Command::none())
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        (
+            AudioVisualizer {
+                file_path: None,
+                audio_output: Some(stream_handle),
+                audio_sink: None,
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -50,7 +62,22 @@ impl Application for AudioVisualizer {
                 }
             }
             Message::PlayPressed => {
-                println!("Start pressed")
+                println!("Start pressed");
+                self.file_path.as_ref().and_then(|path| {
+                    self.audio_output.as_ref().and_then(|audio_output| {
+                        File::open(path).ok().and_then(|file| {
+                            Decoder::new(std::io::BufReader::new(file))
+                                .ok()
+                                .map(|source| {
+                                    audio_output
+                                    .play_raw(source.convert_samples())
+                                    .map_err(|e| {
+                                        println!("Error playing audio: {:?}", e);
+                                    })
+                                })
+                        })
+                    })
+                });
             }
         }
 
@@ -70,7 +97,11 @@ impl Application for AudioVisualizer {
         let file_text = Text::new(file_name);
         let play_button = Button::new(Text::new("Play")).on_press(Message::PlayPressed);
 
-        Column::new().push(open_button).push(file_text).push(play_button).into()
+        Column::new()
+            .push(open_button)
+            .push(file_text)
+            .push(play_button)
+            .into()
     }
 
     fn theme(&self) -> Self::Theme {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,21 @@ mod audio_visualizer;
 use audio_visualizer::AudioVisualizer;
 use iced::{Application, Settings};
 
+// TODO remove debug import  
+use cpal::traits::{DeviceTrait, HostTrait};
+
 fn main() -> iced::Result {
+    list_output_devices();
     AudioVisualizer::run(Settings::default())
+}
+
+// TODO remove debug code  
+fn list_output_devices() {
+    let host = cpal::default_host();
+    let output_devices = host.output_devices().unwrap();
+
+    println!("Available output devices:");
+    for (index, device) in output_devices.enumerate() {
+        println!("{}. {}", index + 1, device.name().unwrap());
+    }
 }
